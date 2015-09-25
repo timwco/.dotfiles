974e70595e92bd49540032982ae389512ddb06ae
Tim Whitacre <tim@timw.co>
2015-09-25 10:08:01 -0400

    Updated cl messages

--- setup.sh
+++ setup.sh
@@ -12,21 +12,21 @@ olddir=~/.dotfiles_old
 files="gitconfig zshrc spacemacs zpreztorc"
 
 # create dotfiles_old in homedir
-echo "\nCreating $olddir for backup of any existing dotfiles in ~"
+echo "\n1. Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 
 # change to the dotfiles directory
-echo "\nChanging to the $templates directory"
+echo "\n2. Changing to the $templates directory"
 cd $templates
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo "\n3. Moving existing dotfiles from ~ to $olddir \n"
 for file in $files; do
-    echo "\nMoving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir/
-    echo "\nCreating symlink to $file in home directory."
+    echo "Creating symlink for $file"
     ln -s $templates/$file ~/.$file
 done
 
 # Copy over Sublime Text 3 Settings
-echo "\nCopying Over Sublime Text 3 Info"
+echo "\n4. Copying Over Sublime Text 3 Info"
 ln -sf $extras/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
